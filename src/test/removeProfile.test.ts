828c2669ebd0a631c17045d083ed129eb4241168
Aaron Powell <me@aaron-powell.com>
2019-07-03 14:02:52 +1000

    test for removing extensions from profile

--- src/test/removeProfile.test.ts
+++ src/test/removeProfile.test.ts
@@ -1,12 +1,21 @@
 /* eslint @typescript-eslint/no-explicit-any: "off" */
 import { assert } from "chai";
 import * as vscode from "vscode";
-import { ConfigKey, ConfigProfilesKey, ConfigStorageKey } from "../constants";
+import {
+  ConfigKey,
+  ConfigProfilesKey,
+  ConfigStorageKey,
+  ConfigExtensionsKey
+} from "../constants";
 import Config from "../services/config";
+import { ExtensionInfo } from "../services/extensions";
 
 suite("remove profile", () => {
   const expectedProfileName = "test1";
   const expectedProfileSettings = { foo: "bar" };
+  const expectedExtensions = [
+    new ExtensionInfo("abcd", "test.ext", "test", "1.0.0", "ext")
+  ];
 
   setup(async () => {
     let config = vscode.workspace.getConfiguration(ConfigKey);
@@ -20,6 +29,13 @@ suite("remove profile", () => {
       ConfigStorageKey,
       {
         [expectedProfileName]: expectedProfileSettings
+      },
+      vscode.ConfigurationTarget.Global
+    );
+    await config.update(
+      ConfigExtensionsKey,
+      {
+        [expectedProfileName]: expectedExtensions
       },
       vscode.ConfigurationTarget.Global
     );
@@ -43,5 +59,15 @@ suite("remove profile", () => {
     let settings = config.getProfileSettings(expectedProfileName);
 
     assert.isUndefined(settings);
+  });
+
+  test("can remove profile extensions", async () => {
+    let config = new Config();
+
+    await config.removeProfileExtensions(expectedProfileName);
+
+    let extensions = config.getProfileExtensions(expectedProfileName);
+
+    assert.isEmpty(extensions);
   });
 });
